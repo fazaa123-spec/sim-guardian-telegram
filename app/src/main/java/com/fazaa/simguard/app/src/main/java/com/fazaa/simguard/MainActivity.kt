e9b4b581e6e9807f347fb84f35386c84a673e0e5
muatez Mhgoub Margani <muatezfazaa123@gmail.com>
2026-09-24 12:54:03 +0300

    Update MainActivity.kt

--- app/src/main/java/com/fazaa/simguard/app/src/main/java/com/fazaa/simguard/MainActivity.kt
+++ app/src/main/java/com/fazaa/simguard/app/src/main/java/com/fazaa/simguard/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.fazaa.simguard
-
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,21 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
         val prefs = getSharedPreferences("guard", MODE_PRIVATE)
-        val tokenInput = findViewById<EditText>(R.id.token)
-        val chatInput = findViewById<EditText>(R.id.chatId)
-        val btn = findViewById<Button>(R.id.btnSave)
+        prefs.edit()
+            .putString("bot_token", "8982745979:AAG-SdnF_tkkYzoTvov8E7WJF_v0fW3lL4I")
+            .putString("chat_id", "6635794096")
+            .apply()
 
-        tokenInput.setText(prefs.getString("bot_token",""))
-        chatInput.setText(prefs.getString("chat_id",""))
-
-        btn.setOnClickListener {
-            prefs.edit()
-                .putString("bot_token", tokenInput.text.toString())
-                .putString("chat_id", chatInput.text.toString())
-                .apply()
-            Toast.makeText(this, "تم تفعيل الحماية بنجاح ✅", Toast.LENGTH_LONG).show()
+        findViewById<Button>(R.id.btnSave).setOnClickListener {
+            Toast.makeText(this, "تم تفعيل الحماية ✅", Toast.LENGTH_LONG).show()
         }
     }
 }
