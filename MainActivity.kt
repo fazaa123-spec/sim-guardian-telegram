aab3fa9956f9dc30aee8fc539b16708bf57839c7
muatez Mhgoub Margani <muatezfazaa123@gmail.com>
2026-09-24 12:30:46 +0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,23 @@
+package com.fazaa.simguard
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val prefs = getSharedPreferences("guard", MODE_PRIVATE)
+        // نحفظ القيم بتاعتك تلقائي أول مرة
+        prefs.edit()
+            .putString("bot_token", "8982745979:AAG-SdnF_tkkYzoTvov8E7WJF_v0fW3lL4I")
+            .putString("chat_id", "6635794096")
+            .apply()
+
+        findViewById<Button>(R.id.btnSave).setOnClickListener {
+            Toast.makeText(this, "الحماية مفعلة ✅ هتجيلك رسالة على تليجرام لو الشريحة اتغيرت", Toast.LENGTH_LONG).show()
+        }
+    }
+}
